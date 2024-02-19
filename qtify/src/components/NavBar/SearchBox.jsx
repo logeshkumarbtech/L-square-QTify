e9e624613e49fc9c7ea91f3772d9e5f955b5bf1c
Logesh kumar G <logeshkumarbtech@gmail.com>
2024-02-19 14:20:43 +0530

    Test failure fix

--- qtify/src/components/NavBar/SearchBox.jsx
+++ qtify/src/components/NavBar/SearchBox.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { TextField, InputAdornment } from "@mui/material";
 import styles from "./SearchBox.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 
 export default function SearchBox() {
-  return (
-    // <div className={styles.parent}>
-    //   <TextField      
-    //     className={styles.search}
-    //     InputProps={{
-    //       endAdornment: (
-    //         <InputAdornment position="end">
-    //           <SearchIcon />
-    //         </InputAdornment>
-    //       ),
-    //     }}
-    //     placeholder="Search for song of your own choice"
-    //     name="search"
-    //   />
-    // </div>
-    <div className={styles.parent}>
+  return (    
+    <form className={styles.parent}>
       <input        
         className={styles.search}
         placeholder="Search a album of your choice"
@@ -27,6 +12,6 @@ export default function SearchBox() {
       <button className={styles.searchButton} type="submit">
         <SearchIcon />
       </button>
-    </div>
+    </form>
   );
 }
